3b3926647dc4b706fdaf5bd3a2f927466c925b0a
Shiba <masashibaprograming@gmail.com>
2021-02-14 21:41:53 +0900

    着手

---
+++ script.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: "#logos"
+})
+
+new Vue({
+  el:"#bgimgs",
+  data: {
+    imgs:[
+      {index:0,feature:"black_square",width:302,top:-72,left:119},
+      {index:1,feature:"orange_rectangle",width:290,top:202,left:1673},
+      {index:2,feature:"pink_circle",width:183,top:922,left:1116},
+      // {index:3,feature:"white_triangle",width:232,top:1149,left:-8},
+      {index:4,feature:"lightgreen_square",width:284,top:1342,left:1794},
+      {index:5,feature:"orange_circle",width:184,top:2196,left:363},
+      {index:6,feature:"blue_rectangle",width:378,top:2669,left:1664},
+      {index:7,feature:"green_triangle",width:191,top:3220,left:73},
+    ]
+  }
+})
