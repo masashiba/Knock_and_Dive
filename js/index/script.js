4207bd145d3e1c90840adbdc4153bddaa3baba8a
cannot-fly-pig <document.sabakan@gmail.com>
2021-02-28 17:08:16 +0900

    予約の説明

---
+++ js/index/script.js
@@ -0,0 +1,121 @@
+explanationBox = {
+  props: ["title","message"],
+  template: `<div style="position: relative">
+              <img src="./img/box_nothing.svg" style="width: 100%;">
+              <p :style="[titleStyle,{fontSize: width * 0.04 + 'px'}]"><span v-html="title"></span></p>
+              <div :style="[messageStyle,{fontSize: width * 0.43 / Math.sqrt(messagelen) + 'px'}]">
+                <p><span v-html="message"></span></p>
+              </div>
+            </div>`,
+  data: function(){
+    return {
+      width: 0,
+      titleStyle: {
+        position: 'absolute',
+        top: '1.6%',
+        left: '-0.2%',
+        width: '50%',
+        WebkitTransform: 'rotate(-8.3deg)',
+                transform: 'rotate(-8.3deg)',
+        fontFamily: "GenShinGothic-Heavy",
+        textAlign: "center"
+      },
+      messageStyle: {
+        position: 'absolute',
+        width: '63.1%',
+        top: '18.9%',
+        left: '9.5%',
+        padding: '0 12.5% 0 13%',
+        fontFamily: "GenShinGothic-Medium"
+      }
+    }
+  },
+  computed: {
+    messagelen: function(){
+      return this.message.length
+    }
+  },
+  methods: {
+    getTargetWidth: function() {
+      this.width = this.$el.clientWidth
+    }
+  },
+  mounted: function(){
+    this.getTargetWidth()
+    window.addEventListener('resize', this.getTargetWidth)
+  }
+}
+
+new Vue({
+  el: "#logos"
+})
+
+new Vue({
+  el:"#bgimgs",
+  data: {
+    imgs:[
+      {index:0,name:"black_square.svg",width:302,top:-72,left:119},
+      {index:1,name:"orange_rectangle.svg",width:290,top:202,left:1673},
+      {index:2,name:"pink_circle.svg",width:183,top:922,left:1116},
+      {index:3,name:"white_triangle.svg",width:232,top:1149,left:-8},
+      {index:4,name:"lightgreen_square.svg",width:284,top:1342,left:1794},
+      {index:5,name:"orange_circle.svg",width:184,top:2196,left:363},
+      {index:6,name:"blue_rectangle.svg",width:378,top:2669,left:1664},
+      {index:7,name:"green_triangle.svg",width:191,top:3220,left:73},
+      {index:8,name:"pink_circle.svg",width:183,top:4093,left:1761},
+      {index:9,name:"orange_rectangle.svg",width:290,top:4546,left:-35},
+      {index:10,name:"yellow_triangle1.png",width:100,top:3892,left:1493},
+      {index:11,name:"yellow_triangle2.png",width:100,top:3919,left:288},
+      {index:12,name:"yellow_triangle3.png",width:100,top:4410,left:1668},
+      {index:13,name:"yellow_triangle4.png",width:100,top:4507,left:275},
+      {index:14,name:"yellow_triangle5.png",width:100,top:4703,left:1211}
+    ]
+  },
+  methods: {
+    scrollTop: function(){
+      window.scrollTo({
+        top: 10,
+        behavior: "smooth"
+      });
+    }
+  },
+  mounted: function(){
+    this.scrollTop()
+  }
+})
+
+new Vue({
+  el:"#explanation",
+  components: {
+     'explanation-box': explanationBox
+  },
+  data: {
+    explanations: [
+      {
+        id: 0,
+        title: "オンライン文化祭",
+        message: "今年は、校舎とオンラインの同時開催。<br>部誌や展示物の公開、YouTubeチャンネルでの動画企画をはじめ、幅広い企画を５月２・３日に公開します。詳しくは、公式サイトやSNSで告知いたします。<br>あなたも、灘校文化祭の世界へKnock＆Dive！"
+      },
+      {
+        id: 1,
+        title: "予約準備中",
+        message: "“Knock&Dive”では、校舎への入場は、事前の予約が必要です。<br>応募期間は、３月20日から４月２日。応募者多数の場合は、抽選を行います。<br>詳しくはリンクをクリック！"
+      }
+    ]
+  }
+})
+
+new Vue({
+  el:"#countdown",
+  computed: {
+    maysecond: function(){
+      return new Date(2021, 4, 2, 0, 0, 0)
+    },
+    now: function(){
+      return Date.now()
+    },
+    left: function(){
+      return Math.ceil((this.maysecond - this.now)/(1000 * 60 * 60 * 24))
+    }
+  }
+})
---
+++ js/reservation_detail/phone.js
@@ -0,0 +1,115 @@
+reservationExplain = {
+  props: ["number"],
+  template: `<div :style="style.container">
+							<div :style="style.numberContainer"><span style="font-size: 1rem;">step</span><span style="font-size: 1.5rem;">{{ number }}</span></div>
+							<div :style="style.imgContainer">
+								<img :src="explains[number-1].img" v-bind:alt="explains[number-1].title" :style="style.reservationimg">
+								<div :style="style.reservationtitle">{{ explains[number-1].title }}</div>
+							</div>
+							<div :style="style.messageContainer">
+								<ul v-for="message in explains[number-1].messages" :style="style.message">
+									<li v-html="message"></li>
+								</ul>
+							</div>
+            </div>`,
+  data: function(){
+    return {
+			explains: [
+				{
+					title: "入場予約フォームで応募",
+					messages: [
+						"1組あたり上限3人までとなります。",
+						"小学生以下の方のみで入場することはできません。保護者の方の同伴をお願いします。",
+						"3/20から4/2の間に入場予約フォームから応募してください。"
+					],
+					img: "./img/form.svg"
+				},
+				{
+					title: "デジタルチケットの添付されたメールを受信",
+					messages: [
+						"応募者多数の場合は抽選をします。",
+						"メールは、4月に配信予定です。"
+					],
+					img: "./img/mail.svg"
+				},
+				{
+					title: "イベントの予約",
+					messages: [
+						"4月中旬、イベント参加の事前予約をします。"
+					],
+					img: "./img/clipboard.svg"
+				},
+				{
+					title: "校舎へ入場！",
+					messages: [
+						"受付ブースでデジタルチケットを提示してください。<br>灘校文化祭の世界へKnock&Dive!"
+					],
+					img: "./img/school.svg"
+				},
+			],
+			style: {
+				container: {
+					'display': 'flex',
+					'flex-direction': 'row',
+					'position': 'relative',
+					'width': '90vw',
+					'height': '28vh',
+					'border': 'solid #5CB4EF 2px',
+					'border-radius' : '20% / 50%',
+					'box-shadow': '10px 15px 0px 0 #ADADAD'
+				},
+				numberContainer: {
+					'display': 'flex',
+					'flex-direction': 'row',
+					'align-items': 'center',
+					'position': 'absolute',
+					'height': '2rem',
+					'top': '-5%',
+					'left': '0',
+					'color': 'white',
+					'background': '#ED9A5D',
+					'padding': '0.2rem 1rem',
+					'border-radius' : '20% / 50%'
+				},
+				imgContainer: {
+					'display': 'flex',
+					'width': '35%',
+					'height': '100%',
+					'padding-left': '10%',
+					'flex-direction': 'column',
+					'justify-content': 'space-around',
+					'align-items': 'center',
+					'background': '#5CB4EF',
+					'border-radius' : '40% 0 0 40% / 50% 0 0 50%'
+				},
+				reservationimg: {
+					'max-height': '75%',
+					'max-width': '60%'
+				},
+				reservationtitle: {
+					'max-width': '80%',
+					'color': 'white',
+					'font-size': '0.8rem'
+				},
+				messageContainer: {
+					'width': '50%',
+					'height': '100%',
+					'padding-top': '3%',
+					'padding-bottom': '3%',
+				},
+				message: {
+					'width': '75%',
+					'font-size': '0.6rem',
+					'padding-left': '1.5rem',
+				}
+			}
+    }
+  },
+}
+
+new Vue({
+  el:"#wrapper",
+  components: {
+     'reservation-explain': reservationExplain
+  },
+})
